cd13bd0103c0a93ca836d98a4a908066be5796f6
LoveLakhwani <86642913+lakhwaniji@users.noreply.github.com>
2021-08-26 10:09:47 +0530

    Completed

--- src/Robots.js
+++ src/Robots.js
@@ -4,38 +4,38 @@ export const Robots=[
         id:1,
         name:"Saurabh",
         fullname:"Saurabh Lakhwani",
-        contactno:9982859488,
+        contactno:'+91 9982859488',
         email:"souravlakhwani325@gmail.com",
         age:19,
-        Birthday:""
+        Birthday:"3-6-2002"
     },
     {
         id:2,
         name:"Love",
         fullname:"Love Lakhwani",
-        contactno:8171062253,
+        contactno:'+91 8171062253',
         email:"lovelakhwani181@gmail.com",
         age:19,
-        Birthday:""
+        Birthday:"16-12-2002"
     },
     {
         
         id:3,
         name:"Prince",
         fullname:"Prince Lakhwani",
-        contactno:1234567890,
+        contactno:'+91 7410949251',
         email:"lovelakhwani181@gmail.com",
         age:19,
-        Birthday:""
+        Birthday:"16-3-2006"
     },
     {
         id:4,
         name:"Advit",
         fullname:"Advit Lakhwani",
-        contactno:1234567890,
+        contactno:'+91 9389031079',
         email:"advitlankhwani@gmail.com",
         age:11,
-        Birthday:""
+        Birthday:"8-3-2010"
 
     },
 
@@ -43,10 +43,10 @@ export const Robots=[
         id:5,
         name:"Anand",
         fullname:"Anand Narwaani ",
-        contactno:1234567890,
+        contactno:'+971 50 645 0029',
         email:"anandnarwani454@gmail.com",
         age:7,
-        Birthday:""
+        Birthday:"7-8-2012"
 
 
     },
@@ -54,27 +54,27 @@ export const Robots=[
         id:6,
         name:"Angad",
         fullname:"Angad Narwani",
-        contactno:1234567890,
+        contactno:'Too small To have a number',
         email:"angadnarwani454@gmail.com",
-        age:1.5,
-        Birthday:""
+        age:1,
+        Birthday:"23-9-2020"
 
     },
     {
         id:7,
         name:"Ananyaa",
         fullname:"Ananya ----",
-        contactno:1234567890,
+        contactno:'+91 89494 311094',
         email:"ananya454@gmail.com",
         age:12,
-        Birthday:""
+        Birthday:"Not Given"
 
     },
     {
         id:8,
         name:"Drishti",
         fullname:"Drishti -------",
-        contactno:1234567890,
+        contactno:'Too small to have a number',
         email:"Drishti444@gmail.com",
         age:7,
         Birthday:""
@@ -86,10 +86,10 @@ export const Robots=[
         id:9,
         name:"Neeraj",
         fullname:"Neeraj Narwani",
-        contactno:1234567890,
+        contactno:'+971 55 553 7590',
         email:"neerajnarwani@gmail.com",
-        age:35,
-        Birthday:""
+        age:38,
+        Birthday:"22-9-1983"
 
     },
     {
@@ -97,10 +97,10 @@ export const Robots=[
         id:10,
         name:"Muskan",
         fullname:"Muskan Narwani",
-        contactno:1234567890,
+        contactno:'+971 55 894 5813',
         email:"muskannarwani123@gmail.com",
-        age:34,
-        Birthday:""
+        age:36,
+        Birthday:"28-2-1985"
 
 
     },
@@ -109,10 +109,10 @@ export const Robots=[
         id:11,
         name:"Prakash",
         fullname:"Prakash Lakhwani",
-        contactno:1234567890,
+        contactno:'+91 9414728684',
         email:"prakashlakhwani454@gmail.com",
-        age:45,
-        Birthday:""
+        age:44,
+        Birthday:"8-11-1977"
 
     },
     {
@@ -120,10 +120,10 @@ export const Robots=[
         id:12,
         name:"Sapna",
         fullname:"Sapna Lakhwani",
-        contactno:1234567890,
+        contactno:'+91 63678 34922',
         email:"sapnalakhwani123@gmail.com",
-        age:42,
-        Birthday:""
+        age:44,
+        Birthday:"18-12-1977"
 
     },
     {
@@ -131,10 +131,10 @@ export const Robots=[
         id:13,
         name:"Naveen",
         fullname:"Naveen ji",
-        contactno:1234567890,
+        contactno:'+91 707145 25764',
         email:"naveen45454@gmail.com",
         age:38,
-        Birthday:""
+        Birthday:"Not Given"
 
     },
     {
@@ -145,7 +145,7 @@ export const Robots=[
         contactno:1234567890,
         email:"Bharti4123@gmail.com",
         age:37,
-        Birthday:""
+        Birthday:"Not given"
 
     },
     {
@@ -153,10 +153,10 @@ export const Robots=[
         id:15,
         name:"Indramohan",
         fullname:"Indramohan Lakhwani",
-        contactno:9557589020,
+        contactno:'+91 9557589020',
         email:"indramohanlakhwani@gmail.com",
-        age:41,
-        Birthday:""
+        age:43,
+        Birthday:"19-8-1978"
     },
     {
         
@@ -165,8 +165,8 @@ export const Robots=[
         fullname:"Manju Lakhwani",
         contactno:9389031079,
         email:"priyalakhwani1214@gmail.com",
-        age:40,
-        Birthday:""
+        age:41,
+        Birthday:"24-8-1981"
 
     }
 ];
